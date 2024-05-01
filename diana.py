ac57f6a5a868ed44a337aab4033929b8ce60b464
Courtney Bell <courtneyimbert@gmail.com>
2024-05-01 14:16:39 -0400

    Update diana.py

--- diana.py
+++ diana.py
@@ -4,9 +4,6 @@
 # Courtney Imbert, courtneyimbert@gmail.com
 #
 # This short program is a python implementation of the polyalphabetic, symmetric Diana Cryptosystem, in use by U.S. Special Forces during the Vietnam War. It encrypts/decrypts strings using a One Time Pad (OTP) or key. The algorithm is similar to a Vernam or Vigenere cipher.
-#
-# Later I may add features like stripping non-ascii characters in the plaintext (other than spaces), generating random OTP values, specifying pre-shared parts of the OTP to start from, etc.
-#
 # For more information on the Diana Cryptosystem, read http://www.blogbyben.com/2014/12/trigraphs-diana-pads-and-zombies.html
 # 
 # This program is for personal hobbyist and puzzle use. It is NOT a secure method for encrypting data. I wrote it for fun.
